57a299be6c47bdd9bb4d277977c0de904aa25a90
saile336 <elias.nybondas@gmail.com>
2023-05-08 13:35:06 +0000

    msg

--- app.py
+++ app.py
@@ -11,18 +11,48 @@ app.config['JSON_AS_ASCII'] = False
 load_dotenv()
 db_url = os.environ.get("DB_URL")
 
+conn = psycopg.connect(db_url, row_factory=dict_row, autocommit=True) #dict_row: få query-resultat som list of dicts
+
 @app.route("/")
 def index():
-    return { "message": "Hello" }
+    return { "message": "Use /todo for API endpoint" }
 
-@app.route("/todo", methods=["GET"])
+@app.route("/todo", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
 def get_todos():
-    conn = psycopg.connect(db_url)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM todos")
-    todos = cursor.fetchall()
-    conn.close()
-    return jsonify(todos)
+    if request.method == 'GET':
+        with conn.cursor() as cur:
+            cur.execute("SELECT todo.*, categories.category_name FROM todo INNER JOIN categories on todo.category_id = categories.category_name ")
+            result = cur.fetchall()
+        return { "bookings": result }
+
+    if request.method == 'POST':
+        try:
+            req_body = request.get_json()
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO hotel_booking (
+                        room_id,
+                        guest_id,
+                        datefrom,
+                        dateto, 
+                        addinfo
+                    ) VALUES (
+                        %s, %s, %s, %s, %s
+                    ) RETURNING id
+                """, [
+                req_body['room_id'], 
+                req_body['guest_id'], 
+                req_body['datefrom'], 
+                req_body['dateto'],
+                req_body['addinfo']
+                ])
+                return {"id": cur.fetchone()['id']}
+        except Exception as e:
+            print(repr(e))
+            return { "ERROR, check logs for details" }, 400
+
+    else:
+        return { "Du använde metoden": request.method }
 
 
 ## Kom ihåg:
@@ -31,4 +61,4 @@ def get_todos():
 # - Ändra portnummer nedan
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8448, debug=True)
+    app.run(host='0.0.0.0', port=8322, debug=True)
