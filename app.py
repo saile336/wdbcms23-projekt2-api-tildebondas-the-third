e79111a696050c40c662092d5090a1dfd3553fc5
zorixxe <robin@tildeman.ax>
2023-05-10 08:01:15 +0000

    api key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg
 from psycopg.rows import dict_row
-from flask import Flask, request, jsonify, escape
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -20,27 +20,43 @@ conn = psycopg.connect(db_url, row_factory=dict_row, autocommit=True)
 def index():
     return {"message": "Use /todo for API endpoint"}
 
+def check_key(api_key):
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT id
+            FROM users
+            WHERE api_key = %s""",
+            [api_key])
+        return cur.fetchone()['id']
+    
+@app.route("/users/<int:id>")
+def user(id):
+    try:
+        user_id = check_key(request.args.get('api_key'))
+    except:
+        return {"message": "ERROR: Invalid API-key"}, 401
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT * 
+                FROM users
+                WHERE id = %s""", [ user_id ])
+            result = cur.fetchone()
+        return result or {"message": "ERROR: no such guest"}, 400
+    except:
+        # vi kan skriva statuskoden direkt efter response bodyn
+        return {"message": "ERROR: something went wrong"}, 400
+
 
 @app.route("/todo", methods=["GET", "POST"])
 def get_todos():
     if request.method == 'GET':
         with conn.cursor() as cur:
             cur.execute(
-            """ SELECT todo.id,
-                todo.user_id,
-                todo.title,
-                todo.done,
-                todo.due_date,
-                todo.created_at,
-                todo.updated_at,
-                todo.sort_order,
-                categories.category_name 
-                FROM todo 
-                INNER JOIN categories 
-                ON todo.category_id=categories.id
-                ORDER BY due_date ASC""")
+                "SELECT todo.*, categories.category_name FROM todo INNER JOIN categories ON todo.category_id=categories.id")
             result = cur.fetchall()
-        return jsonify(result)
+        return {"ToDo": result}
 
     if request.method == 'POST':
         try:
@@ -58,12 +74,12 @@ def get_todos():
                 """, [
                     req_body['user_id'],
                     req_body['category_id'],
-                    escape(req_body['title']),
+                    req_body['title'],
                     req_body['due_date'],
                 ])
                 return {"id": cur.fetchone()['id']}
         except Exception as e:
-            print(repr(e))
+            print(e)
             return {"ERROR": "Check logs for details"}, 400
 
     else:
@@ -71,36 +87,40 @@ def get_todos():
 
 
 @app.route("/todo/<int:id>", methods=["PUT", "PATCH", "DELETE"])
-def update_todo(id):
+def update_todo():
     if request.method == "PUT" or request.method == "PATCH":
         try:
             req_body = request.get_json()
             with conn.cursor() as cur:
                 cur.execute("""
-                    UPDATE todo
-                    SET category_id = %s,
-                        title = %s,
-                        due_date = %s
-                    WHERE id = %s;
+                    UPDATE todo 
+                    SET category_id=%s,
+                        title=%s,
+                        due_date=%s
+                    WHERE id=%s
+                    RETURNING id
                 """, [
                     req_body['category_id'],
-                    escape(req_body['title']),
+                    req_body['title'],
                     req_body['due_date'],
-                    id,
+                    req_body['id'],
                 ])
-                return {"updated todo id": id}
+                return {"updated todo id": cur.fetchone()['id']}
 
         except Exception as e:
             print(repr(e))
-            return {"error": "check logs"}, 400
+            return {"ERROR, check logs for details"}, 400
 
     if request.method == "DELETE":
         try:
+            req_body = request.get_json()
             with conn.cursor() as cur:
                 cur.execute("""
-                    DELETE FROM todo WHERE id = %s;
-                """, [id])
-                return {"deleted id": id}
+                    DELETE todo where id=%s RETURNING id
+                """, [
+                    req_body['id']
+                ])
+                return {"deleted id": cur.fetchone()['id']}
 
         except Exception as e:
             print(repr(e))
@@ -108,6 +128,8 @@ def update_todo(id):
 
     else:
         return {"Du använde metoden": request.method}
+    
+
 
 
 # Kom ihåg:
@@ -116,4 +138,4 @@ def update_todo(id):
 # - Ändra portnummer nedan
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8322, debug=True)
+    app.run(host='0.0.0.0', port=8448, debug=True)
